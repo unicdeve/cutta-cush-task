4bb761b89542fabaf13e4399d2ef50b7f290fedb
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-06-23 19:46:27 +0100

    work-shedule.pdf and frontend dev done

---
+++ frontend/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 4000,
+    pagination: false
+  });
+
+  var typed = new Typed('.typed', {
+    strings: ['Your #1 ...', 'online ...', 'solution!', ''],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
+});
